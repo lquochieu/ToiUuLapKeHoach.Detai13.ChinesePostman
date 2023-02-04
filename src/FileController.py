e8521361e9e21553b7a6dddf47210e47f15168d5
Hiệp Trần <69852463+lquochieu@users.noreply.github.com>
2023-02-04 11:31:55 +0700

    fix: IP

--- src/FileController.py
+++ src/FileController.py
@@ -1,11 +1,11 @@
 class FileController:
     @staticmethod
-    def Read_File_Data():
+    def Read_File_Data(filename):
         type = ''
         num_cus = 0
         x = []
         t = []
-        with open('../data/A-n32-k5.txt','r') as f:
+        with open(filename,'r') as f:
             for line in f:
                 word = line.strip().split(' ')
 
@@ -27,8 +27,48 @@ class FileController:
                         x.append((float(word[1]),float(word[2])))
                     elif(type == 'DEMAND_SECTION'):
                         t.append(float(word[1]))
-        
+            f.close()
         return num_cus, x, t
 
+    @staticmethod
+    def Read_Multi_File_Data(filenames):
+        type = ''
+        num_customers = []
+        # customers_coord is the coordinate of custumer. The deliver time between 2 person is the distance euclean between them
+        customers_coords = []
+        # receiving_time is the time what customer receive item
+        receiving_times = []
+        for filename in filenames:
+            num_cus = 0
+            x = []
+            t = []
+            with open(filename,'r') as f:
+                for line in f:
+                    word = line.strip().split(' ')
+
+                    if(word[0] == 'NUM_CUS'):
+                        type = 'NUM_CUS'
+                        continue
+                    elif(word[0] == 'NODE_COORD_SECTION'):
+                        type = 'NODE_COORD_SECTION'
+                        continue
+                    elif(word[0] == 'DEMAND_SECTION'):
+                        type = 'DEMAND_SECTION'
+                        continue
+                    elif(word[0] == 'DEPOT_SECTION'):
+                        break
+                    else:
+                        if(type == 'NUM_CUS'):
+                            num_cus = int(word[0])
+                        elif(type == 'NODE_COORD_SECTION'):
+                            x.append((float(word[1]),float(word[2])))
+                        elif(type == 'DEMAND_SECTION'):
+                            t.append(float(word[1]))
+                num_customers.append(num_cus)
+                customers_coords.append(x)
+                receiving_times.append(t)
+                f.close()
+        
+        return num_customers, customers_coords, receiving_times
                     
 
--- src/IP.py
+++ src/IP.py
@@ -1,31 +1,38 @@
 from FileController import FileController
 from ortools.linear_solver import pywraplp
 
-# customers_coord is the coordinate of custumer. The deliver time between 2 person is the distance euclean between them
-customers_coord = [(82.0, 76.0), (96.0, 44.0), (50.0, 5.0), (49.0, 8.0), (13.0, 7.0), (29.0, 89.0), (58.0, 30.0), (84.0, 39.0), (14.0, 24.0), (2.0, 39.0), (3.0, 82.0), (5.0, 10.0), (98.0, 52.0), (84.0, 25.0), (61.0, 59.0), (1.0, 65.0), (88.0, 51.0), (91.0, 2.0), (19.0, 32.0), (93.0, 3.0), (50.0, 93.0), (98.0, 14.0), (5.0, 42.0), (42.0, 9.0), (61.0, 62.0), (9.0, 97.0), (80.0, 55.0), (57.0, 69.0), (23.0, 15.0), (20.0, 70.0), (85.0, 60.0), (98.0, 5.0)]
-# receiving_time is the time what customer receive item
-receiving_time = [0.0, 19.0, 21.0, 6.0, 19.0, 7.0, 12.0, 16.0, 6.0, 16.0, 8.0, 14.0, 21.0, 16.0, 3.0, 22.0, 18.0, 19.0, 1.0, 24.0, 8.0, 12.0, 4.0, 8.0, 24.0, 24.0, 2.0, 20.0, 15.0, 2.0, 14.0, 9.0]
+def get_data():
+    filenames = []
+    for i in range(32, 40):
+        if i == 35: continue
+        filenames.append(f'../data/A-n{i}-k5.txt')
 
-customers_coord = customers_coord[:9]
-receiving_time = receiving_time[:15]
-#num_postmas is the number of postman
-num_postmans = 5
+    num_postmans, customers_coords, receiving_times = FileController.Read_Multi_File_Data(filenames)
+    
+    num_customers = [5, 7, 9, 10]
+    n = len(num_customers)
+    m = len(num_postmans)
 
-# num_postmans, customers_coord, receiving_time = FileController.Read_File_Data()
-# print(num_postmans, customers_coord, receiving_time)
+    num_postmans[0:n] = [2]*n
+    num_postmans[n:] = [3]*(m - n)   
 
-#num_customers is the number of customers
-num_customers = len(customers_coord)
-print(num_customers)
+    for i in range(m):
+        customers_coords[i] = customers_coords[i][:num_customers[i % n]]
+        receiving_times[i] = receiving_times[i][:num_customers[i % n]]
+
+    return num_postmans, customers_coords, receiving_times
+
 #transport_duration is fuction what calculate time between 2 person between their coordinate
 def transport_duration(customer_x, customer_y):
     s_sq_difference = 0
     for customer_xi,customer_yi in zip(customer_x, customer_y):
         s_sq_difference += (customer_xi - customer_yi)**2    
     distance = s_sq_difference**0.5
-    return int(distance)
+    return distance
 
-def main():
+def solve(num_postman, customers_coord, receiving_time):
+    #num_customers is the number of customers
+    num_customers = len(customers_coord)
     solver = pywraplp.Solver('VRP', pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
     if not solver:
         return
@@ -33,8 +40,9 @@ def main():
     y = {}
     z = {}
     
-
-    for k in range(num_postmans):
+    print("-----------------------------------------------------------")
+    print(f"Start solving: {num_customers} customer, {num_postman} postman")
+    for k in range(num_postman):
         for i in range(num_customers):
             for j in range(num_customers):
                 # x[i, j, k] = 1 if k_th postman go through the path between i_th customer and j_th customer
@@ -42,7 +50,7 @@ def main():
                 # 0 <= x[i, j, k] <= 1 with each any i, j, k
                 x[i, j, k] = solver.IntVar(0, 1, f'x_{i}_{j}_{k}')
 
-    for k in range(num_postmans):
+    for k in range(num_postman):
         for i in range(num_customers):
             # y[i, k] = 1 if k_th postman visited i_th customer
             # y[i, k] =  vice versa
@@ -57,9 +65,9 @@ def main():
 
     for i in range(1, num_customers):
         solver.Add(
-            solver.Sum(x[i, j, k] for j in range(num_customers) for k in range(num_postmans)) == 1
+            solver.Sum(x[i, j, k] for j in range(num_customers) for k in range(num_postman)) == 1
         )
-    for k in range(num_postmans):
+    for k in range(num_postman):
         #Each postman will leave the start point once time
         solver.Add(
              solver.Sum(x[0, i, k] for i in range(1, num_customers)) == 1
@@ -92,7 +100,7 @@ def main():
         #         )
         # solver.Add(z[0, k] == 0)
     
-    for k in range(num_postmans):
+    for k in range(num_postman):
         for i in range(1, num_customers):
             for j in range(1, num_customers):
                 # solver.Add(
@@ -103,7 +111,7 @@ def main():
                     z[i, k] - z[j, k] + num_customers * x[i, j, k] <= num_customers - 1
                 )
     
-    for k in range(num_postmans):
+    for k in range(num_postman):
         solver.Add(
             solver.Sum(x[i, j, k] * (transport_duration(customers_coord[i], customers_coord[j]) + receiving_time[j]) for i in range(num_customers) for j in range(num_customers)) <= res
         )
@@ -113,7 +121,7 @@ def main():
     if status == pywraplp.Solver.OPTIMAL:
         print("Total value = ", solver.Objective().Value())
         print(res.solution_value())
-        for k in range(num_postmans):
+        for k in range(num_postman):
             print("postman: ", k)
             for i in range(num_customers):
                 for j in range(num_customers):
@@ -122,9 +130,19 @@ def main():
         print('Time = ', solver.WallTime(), ' milliseconds')
     else:
         print('The problem does not have an optimal solution.')
+    print("-----------------------------------------------------------")
+def main():
 
+    # customers_coord is the coordinate of custumer. The deliver time between 2 person is the distance euclean between them
+    customers_coords = []
+    # receiving_time is the time what customer receive item
+    receiving_times = []
 
+    num_postmans, customers_coords, receiving_times = get_data()
 
+    for i in range(len(num_postmans)):
+        solve(num_postmans[i], customers_coords[i], receiving_times[i])
+    
 if __name__ == '__main__':
     main()
 
